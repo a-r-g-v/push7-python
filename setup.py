6f3bff668dc912ea5f81e1b0ddd3ba2bfeebaa6e
greyia <info@thun.ws>
2017-04-02 00:04:15 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import setup
 import os
 
 
+long_description = ""
 if os.path.exists('README.md'):
     long_description = open('README.md').read()
 
 
 setup(
     name='push7',
-    version='0.0.2',
+    version='0.0.3',
     description='Python API Client for Push7',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     license="MIT",
     keywords="push7",
     author='a_r_g_v',
